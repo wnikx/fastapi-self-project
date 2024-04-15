d380286ad1c79246b4e8ecdbc1dc31fe3944e978
Nikita Ostapenko <wnikx79@gmail.com>
2024-04-15 13:53:14 +0300

    feat: add auth models.py

---
+++ src/auth/models.py
@@ -0,0 +1,29 @@
+import datetime
+from typing import Annotated
+
+from sqlalchemy import text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from src.models import Base
+
+created_at = Annotated[
+    datetime.datetime,
+    mapped_column(server_default=text("TIMEZONE('utc', now())")),
+]
+updated_at = Annotated[
+    datetime.datetime,
+    mapped_column(
+        server_default=text("TIMEZONE('utc', now())"),
+        onupdate=datetime.datetime.utcnow,
+    ),
+]
+
+
+class Account(Base):
+    """Таблица с информацией о компаниях"""
+
+    __tablename__ = "accounts"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    account_name: Mapped[str]
+    created: Mapped[created_at]
