04de37f268f8e97f2434b1c3f350edfac4536ee9
Nikita Ostapenko <wnikx79@gmail.com>
2024-04-23 19:02:21 +0300

    add doc-string for registration routers

--- src/api/v1/routers/registration.py
+++ src/api/v1/routers/registration.py
@@ -10,8 +10,9 @@ from src.services.registration import check_validation, email_free, finalize_reg
 reg_router = APIRouter(prefix="/auth/api/v1", tags=["Auth"])
 
 
-@reg_router.post("/check_email")
+@reg_router.post("/check-email")
 async def check_email(email: CheckEmailSchema) -> Response:
+    """A router that checks to see if an e-mail is free."""
     email_is_free = await email_free(email)
     if email_is_free:
         return Response(
@@ -26,6 +27,7 @@ async def check_email(email: CheckEmailSchema) -> Response:
 
 @reg_router.post("/sign-up")
 async def sign_up(sign_up_data: SignUpSchema) -> Response:
+    """The router checks for a match between the e-mail and the token that came to the e-mail."""
     is_valid = await check_validation(sign_up_data)
     if is_valid:
         return Response(content="Data is valid", status_code=200)
@@ -34,6 +36,7 @@ async def sign_up(sign_up_data: SignUpSchema) -> Response:
 
 @reg_router.post("/sign-up-complete")
 async def sign_up_complete(sign_up_comp_data: SignUpCompleteSchema) -> Response:
+    """The router completes the registration process"""
     complete_registration = await finalize_registration(sign_up_comp_data)
     if complete_registration:
         return Response(content="Registration was successful", status_code=201)
