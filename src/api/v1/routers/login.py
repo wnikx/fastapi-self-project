33fdb257fe83eeed612d7b41a3587e77c743309b
Nikita Ostapenko <wnikx79@gmail.com>
2024-04-24 10:51:02 +0300

    fix: improved a section of code

--- src/api/v1/routers/login.py
+++ src/api/v1/routers/login.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from fastapi import APIRouter, HTTPException
 
 from src.schemas.login import LogInSchema
@@ -7,7 +9,7 @@ login_router = APIRouter(prefix="/auth/api/v1", tags=["Log-in"])
 
 
 @login_router.post("/log-in")
-async def log_in(log_in_schema: LogInSchema):
+async def log_in(log_in_schema: LogInSchema) -> Optional[dict]:
     token = await verify_data(log_in_schema)
     if token:
         return {"token": token}
