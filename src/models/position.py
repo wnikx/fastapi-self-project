753e4edcc30768e9d18998751866486e7ac2af18
Nikita Ostapenko <wnikx79@gmail.com>
2024-04-27 11:16:14 +0300

    fix: Model Position to ltree table

--- src/models/position.py
+++ src/models/position.py
@@ -1,7 +1,12 @@
-from sqlalchemy.orm import Mapped, relationship
+from sqlalchemy import Column, Index, Integer, Sequence, String, func
+from sqlalchemy.orm import Mapped, foreign, relationship, remote
+from sqlalchemy_utils import Ltree, LtreeType
 
+from src.database.database import async_engine
 from src.models.base import Base
 from src.utils.custom_types import int_pk, str_256
+
+id_seq = Sequence("position_id_seq")
 
 
 class Position(Base):
@@ -9,7 +14,23 @@ class Position(Base):
 
     __tablename__ = "position"
 
-    id: Mapped[int_pk]
+    id = Column(Integer, id_seq, primary_key=True)
     position_title: Mapped[str_256]
+    path = Column(LtreeType, nullable=False)
 
+    parent = relationship(
+        "Position",
+        primaryjoin=(remote(path) == foreign(foreign(func.subpath(path, 0, -1)))),
+        backref="children",
+        viewonly=True,
+    )
     users: Mapped[list["User"]] = relationship(back_populates="position")  # type: ignore  # noqa: PGH003
+
+    def __init__(self, _position_title, parent=None):
+        _id = async_engine.execute(id_seq)
+        self.id = _id
+        self.position_title = _position_title
+        ltree_id = Ltree(str(_id))
+        self.path = ltree_id if parent is None else parent.path + ltree_id
+
+    __table_args__ = (Index("ix_nodes_path", path, postgresql_using="gist"),)
