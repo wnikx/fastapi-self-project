2cc0a55088d10a21e40e12cac0f13a3d73f249cd
Nikita Ostapenko <wnikx79@gmail.com>
2024-04-29 11:52:59 +0300

    fix: class Position

--- src/models/position.py
+++ src/models/position.py
@@ -1,18 +1,7 @@
-from typing import Any
+from sqlalchemy.orm import Mapped, relationship
 
-from sqlalchemy import Column, Index, Integer, Sequence, func
-from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
-from sqlalchemy.orm import Mapped, foreign, relationship, remote
-from sqlalchemy_utils import Ltree, LtreeType
-
-from src.config import settings
 from src.models.base import Base
-from src.utils.custom_types import str_256
-
-id_seq = Sequence("position_id_seq")
-
-async_engine = create_async_engine(settings.DB_URL, echo=False)
-async_session_maker = async_sessionmaker(bind=async_engine)
+from src.utils.custom_types import int_pk, str_256
 
 
 class Position(Base):
@@ -20,22 +9,7 @@ class Position(Base):
 
     __tablename__ = "position"
 
-    id = Column(Integer, id_seq, primary_key=True)
+    id: Mapped[int_pk]
     position_title: Mapped[str_256]
-    path = Column(LtreeType, nullable=False)
 
-    parent = relationship(
-        "Position",
-        primaryjoin=(remote(path) == foreign(foreign(func.subpath(path, 0, -1)))),
-        backref="children",
-        viewonly=True,
-    )
     users: Mapped[list["User"]] = relationship(back_populates="position")  # type: ignore  # noqa: PGH003
-
-    def __init__(self, id: int, position_title, parent=None):
-        self.id = id
-        self.position_title = position_title
-        ltree_id = Ltree(str(id))
-        self.path = ltree_id if parent is None else parent.path + ltree_id
-
-    __table_args__ = (Index("ix_nodes_path", path, postgresql_using="gist"),)
